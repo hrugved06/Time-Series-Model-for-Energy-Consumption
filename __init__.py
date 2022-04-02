e29c2e017aac6cabdae60b646ae18048657abd5f
Hrugved Kolhe <geeky.hrugved143@gmail.com>
2022-04-03 02:50:43 +0530

    use of __init__ file added

---
+++ __init__.py
@@ -0,0 +1,7 @@
+"""
+__init__.py file treats the functions created in another .py files 
+like a library/package. So we could import them to any other python file.
+
+But, after python 3.3 version it is not necessary to specify 
+the __init__.py file, python implicitly imports the functions from another .py files.
+"""
